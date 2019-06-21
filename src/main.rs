ba261a7bc08b2cfbbecb4db770fcd2c0451d21e0
Martin Baumgart <martin.baumgart@daparto.com>
2019-06-21 19:20:10 +0200

    resolve texture scaling and text positioning

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,8 @@ use piston_window::*;
 use sprite::*;
 use std::rc::Rc;
 
-
 fn main() {
-    let (width, height) = (800, 500);
+    let (width, height) = (400, 600);
     let mut window: PistonWindow = WindowSettings::new("Tetris Game", [width, height])
         .exit_on_esc(true)
         .samples(4)
@@ -23,7 +22,6 @@ fn main() {
         .for_folder("assets")
         .unwrap();
 
-    let id_block;
     let mut scene = Scene::new();
 
     let mut glyphs = window
@@ -31,6 +29,20 @@ fn main() {
         .unwrap();
 
     let mut texture_context = window.create_texture_context();
+
+    let tex_mainboard = Rc::new(
+        Texture::from_path(
+            &mut texture_context,
+            assets.join("mainboard.png"),
+            Flip::None,
+            &TextureSettings::new().mag(Filter::Nearest),
+        )
+        .unwrap(),
+    );
+    let mut sprite_mainboard = Sprite::from_texture(tex_mainboard.clone());
+    sprite_mainboard.set_scale(0.5, 0.5);
+    sprite_mainboard.set_position(width as f64 / 2.0, height as f64 / 2.0);
+    scene.add_child(sprite_mainboard);
 
     let tex_block = Rc::new(
         Texture::from_path(
@@ -42,30 +54,46 @@ fn main() {
         .unwrap(),
     );
     let mut sprite_block = Sprite::from_texture(tex_block.clone());
-    sprite_block.set_scale(4.0, 4.0);
+    sprite_block.set_scale(2.0, 2.0);
     sprite_block.set_position(width as f64 / 2.0, height as f64 / 2.0);
-    id_block = scene.add_child(sprite_block);
+    let id_block = scene.add_child(sprite_block);
 
     let anim_move_up = Action(MoveBy(0.1, 0.0, -100.0));
     let anim_move_down = Action(MoveBy(0.1, 0.0, 100.0));
     let anim_move_left = Action(MoveBy(0.1, -100.0, 0.0));
     let anim_move_right = Action(MoveBy(0.1, 100.0, 0.0));
 
+    let mut cursor = [0.0, 0.0];
+
     // update loop
     while let Some(event) = window.next() {
         scene.event(&event);
 
-        if let Some(Button::Keyboard(key)) = event.press_args() {
-            match key {
-                Key::W => scene.run(id_block, &anim_move_up),
-                Key::S => scene.run(id_block, &anim_move_down),
-                Key::A => scene.run(id_block, &anim_move_left),
-                Key::D => scene.run(id_block, &anim_move_right),
-                _ => println!(""),
+        if let Some(button) = event.release_args() {
+            match button {
+                Button::Keyboard(key) => println!("Released keyboard key '{:?}'", key),
+                Button::Mouse(button) => println!("Released mouse button '{:?}'", button),
+                Button::Controller(button) => println!("Released controller button '{:?}'", button),
+                Button::Hat(hat) => println!("Released controller hat `{:?}`", hat),
             }
         };
 
-        window.draw_2d(&event, |context, graphics, _device| {
+        if let Some(Button::Keyboard(key)) = event.press_args() {
+            match key {
+                Key::Up => scene.run(id_block, &anim_move_up),
+                Key::Down => scene.run(id_block, &anim_move_down),
+                Key::Left => scene.run(id_block, &anim_move_left),
+                Key::Right => scene.run(id_block, &anim_move_right),
+                _ => (),
+            }
+        };
+
+        event.mouse_cursor(|pos| {
+            cursor = pos;
+            println!("Mouse moved '{} {}'", pos[0], pos[1]);
+        });
+
+        window.draw_2d(&event, |context, graphics, device| {
             clear([0.8; 4], graphics);
 
             // rectangle(
@@ -75,19 +103,19 @@ fn main() {
             //     graphics,
             // );
 
-            // draw(&context, &mut graphics);
             scene.draw(context.transform, graphics);
 
-            Text::new_color([0.0, 1.0, 0.0, 1.0], 32)
+            let text_transform = context.transform.trans(100.0, 100.0);
+            Text::new_color([0.0, 0.0, 1.0, 1.0], 32)
                 .draw(
                     "Hello world!",
                     &mut glyphs,
                     &context.draw_state,
-                    context.transform,
+                    text_transform,
                     graphics,
                 )
                 .unwrap();
-            glyphs.factory.encoder.flush(_device);
+            glyphs.factory.encoder.flush(device);
         });
     }
 }

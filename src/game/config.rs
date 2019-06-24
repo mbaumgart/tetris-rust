a976496fabcfd7bbf22b7e01545494a980d97a8b
Martin Baumgart <martin.baumgart@daparto.com>
2019-06-24 15:49:44 +0200

    add configuration for brick types

--- src/game/config.rs
+++ src/game/config.rs
@@ -1,7 +1,14 @@
+use std::collections::HashMap;
+
 pub struct Config {
     pub window_title: String,
     pub window_width: u32,
     pub window_height: u32,
+    pub bricks_horizontal: u8,
+    pub bricks_vertical: u8,
+    pub gravity: f32,
+    pub accelerated_gravity: f32,
+    pub brick_shapes: HashMap<BrickTypes, [[bool; 4]; 4]>,
 }
 
 impl Config {
@@ -10,6 +17,91 @@ impl Config {
             window_title: String::from("Tetris Game"),
             window_width: 400,
             window_height: 800,
+            bricks_horizontal: 10,
+            bricks_vertical: 18,
+            gravity: 0.015,
+            accelerated_gravity: 30.0,
+            brick_shapes: create_brick_shapes(),
         }
     }
+}
+
+#[derive(Hash, Eq, PartialEq)]
+pub enum BrickTypes {
+    L,
+    J,
+    D,
+    S,
+    Z,
+    I,
+    O,
+}
+
+fn create_brick_shapes() -> HashMap<BrickTypes, [[bool; 4]; 4]> {
+    let mut layouts = HashMap::new();
+    layouts.insert(
+        BrickTypes::L,
+        [
+            [false, false, false, false],
+            [true, true, true, false],
+            [true, false, false, false],
+            [false, false, false, false],
+        ],
+    );
+    layouts.insert(
+        BrickTypes::J,
+        [
+            [true, false, false, false],
+            [true, true, true, false],
+            [false, false, false, false],
+            [false, false, false, false],
+        ],
+    );
+    layouts.insert(
+        BrickTypes::D,
+        [
+            [false, false, false, false],
+            [true, true, true, false],
+            [false, true, false, false],
+            [false, false, false, false],
+        ],
+    );
+    layouts.insert(
+        BrickTypes::S,
+        [
+            [false, false, false, false],
+            [false, true, true, false],
+            [true, true, false, false],
+            [false, false, false, false],
+        ],
+    );
+    layouts.insert(
+        BrickTypes::Z,
+        [
+            [false, false, false, false],
+            [true, true, false, false],
+            [false, true, true, false],
+            [false, false, false, false],
+        ],
+    );
+    layouts.insert(
+        BrickTypes::I,
+        [
+            [false, true, false, false],
+            [false, true, false, false],
+            [false, true, false, false],
+            [false, true, false, false],
+        ],
+    );
+    layouts.insert(
+        BrickTypes::O,
+        [
+            [false, false, false, false],
+            [false, true, true, false],
+            [false, true, true, false],
+            [false, false, false, false],
+        ],
+    );
+
+    layouts
 }

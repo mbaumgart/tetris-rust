f15c954a23de3fb773510107fc4e465fa1ec3ff3
Martin Baumgart <martin.baumgart@daparto.com>
2019-06-25 15:04:58 +0200

    add more brick textures

--- src/game/assets.rs
+++ src/game/assets.rs
@@ -9,6 +9,11 @@ pub struct Assets {
     pub font: Glyphs,
     pub brick_blue: G2dTexture,
     pub brick_brown: G2dTexture,
+    pub brick_green: G2dTexture,
+    pub brick_grey: G2dTexture,
+    pub brick_orange: G2dTexture,
+    pub brick_pink: G2dTexture,
+    pub brick_red: G2dTexture,
     pub mainboard: G2dTexture,
 }
 
@@ -26,12 +31,22 @@ impl Assets {
 
         let _brick_blue = load_texture("brick_blue.png", &mut texture_context, &assets_folder);
         let _brick_brown = load_texture("brick_brown.png", &mut texture_context, &assets_folder);
+        let _brick_green = load_texture("brick_green.png", &mut texture_context, &assets_folder);
+        let _brick_grey = load_texture("brick_grey.png", &mut texture_context, &assets_folder);
+        let _brick_orange = load_texture("brick_orange.png", &mut texture_context, &assets_folder);
+        let _brick_pink = load_texture("brick_pink.png", &mut texture_context, &assets_folder);
+        let _brick_red = load_texture("brick_red.png", &mut texture_context, &assets_folder);
         let _mainboard = load_texture("mainboard.png", &mut texture_context, &assets_folder);
 
         Assets {
             font: _font,
             brick_blue: _brick_blue,
             brick_brown: _brick_brown,
+            brick_green: _brick_green,
+            brick_grey: _brick_grey,
+            brick_orange: _brick_orange,
+            brick_pink: _brick_pink,
+            brick_red: _brick_red,
             mainboard: _mainboard,
         }
     }
